2498d8e2ec25b73f739e40dc70e65bc90102fcaf
Lou-fr <pololulu2@outlook.fr>
2023-12-03 22:43:32 +0100

    add gravity and jump function

--- Assets/thirdpersonmovement.cs
+++ Assets/thirdpersonmovement.cs
@@ -6,20 +6,29 @@ using UnityEngine;
 public class thirdpersonmovement : MonoBehaviour
 {
     public CharacterController controller;
-
     public float speed = 6f;
-
+    public float gravity = -9.81f;
+    Vector3 velocity;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
     public Transform cam;
+    public Transform groundcheck;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 3f;
+    public LayerMask groundmask;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundcheck.position, groundDistance, groundmask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
         if(direction.magnitude >= 0.1f)
         {
             float targatAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -27,7 +36,13 @@ public class thirdpersonmovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targatAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed *Time.deltaTime); 
+            controller.Move(moveDir.normalized * speed *Time.deltaTime);
         }
+        if (Input.GetButtonDown("Jump")&& isGrounded) 
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
