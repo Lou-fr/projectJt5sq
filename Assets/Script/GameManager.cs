ac4b578d5c16cf2a650272cfcebb657549fdca6b
Lou-fr <pololulu2@outlook.fr>
2023-12-23 11:28:27 +0100

    Connection system -->ok

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,15 +4,5 @@ using SharedLibrary;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    async void Start()
-    {
-        var player = await HttpClient.Get<Player>("https://localhost:44381/player/500");
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
