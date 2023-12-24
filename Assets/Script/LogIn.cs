dc8fce53c7a068a6d4936d0e9c024d6dc8dd4044
Lou-fr <pololulu2@outlook.fr>
2023-12-24 09:46:15 +0100

    SYS Conection --> not working due to Unity skiping code

--- Assets/Script/LogIn.cs
+++ Assets/Script/LogIn.cs
@@ -1,67 +1,78 @@
 using _httpRequest;
 using Assets.Script.Library.Request;
+using Newtonsoft.Json;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
 using TMPro;
+using LoadAndSaveToken;
 using UnityEngine;
+using UnityEngine.Networking;
 public class LogIn : MonoBehaviour
 {
-    private void Start()
+    public GameObject ConectionMenu;
+    public GameObject PlayButton;
+    public GameObject LogOutButton;
+    void Start()
     {
-        AuthResponse token = loadSaved();
+        AuthResponse token = LoadAndSaveToken.LoadAndSaveToken.LoadSaved();
         if (token == null)
         {
             Debug.Log("No first connection register");
         }
         else
         {
-            Debug.Log("Token found" + token.Token);
+            Debug.Log("Token found : " + token.Token);
+            ConectionMenu.SetActive(false);
+            PlayButton.SetActive(true);
+            LogOutButton.SetActive(true);
         }
     }
+    public GameObject ErrorConection;
+    public TextMeshProUGUI _ErrorConection;
     public string URL = @"http://localhost:5292/auth/log";
     public TMP_InputField UsernameLogin;
     public TMP_InputField PasswordLogin;
-    public async void OnLogInButton()
+    public void OnLogInButton()
     {
         Debug.Log(UsernameLogin.text);
-        Debug.Log(PasswordLogin.text);
+        Debug.Log(PasswordLogin.text);  
         AuthRequest tempRequest = new AuthRequest() { Username = UsernameLogin.text, Password = PasswordLogin.text };
-        var token = await HttpClient.Post<AuthResponse>(URL, tempRequest);
-        Debug.Log(token.Token);
-        if (token.Token != null)
+        AuthResponse token = LogInRequest(tempRequest);
+        Debug.Log(token);
+        if (token != null)
         {
-            SaveToken(token);
-            Debug.Log("Connection succes and token saved");
+            if (token.Token != null)
+            {
+                LoadAndSaveToken.LoadAndSaveToken.SaveToken(token);
+                Debug.Log("Connection succes and token saved");
+                ConectionMenu.SetActive(false);
+                PlayButton.SetActive(true);
+                LogOutButton.SetActive(true);
+            }
         }
         else
         {
-            Debug.LogError("Password or Username incorect");
+            Debug.Log("Error see on line function connection string ");
         }
     }
-    public static void SaveToken(AuthResponse response)
+    public void OnLogOutButton()
     {
-        BinaryFormatter bf = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/player.info";
-        FileStream stream = new FileStream(path, FileMode.Create);
-        Debug.Log(path);
+        LoadAndSaveToken.LoadAndSaveToken.LogOut();
+        ConectionMenu.SetActive(true);
+        PlayButton.SetActive(false);
+        LogOutButton.SetActive(false);
+    }
+    public AuthResponse LogInRequest(AuthRequest request)
+    {
+        var uwr = new UnityWebRequest(URL, "POST");
+        var jsonData = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(request));
+        uwr.uploadHandler = new UploadHandlerRaw(jsonData);
+        uwr.downloadHandler = new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "application/json");
+        uwr.SendWebRequest();
+        return JsonConvert.DeserializeObject<AuthResponse>(uwr.downloadHandler.text);
         
-        bf.Serialize(stream,response);
-        stream.Close();
-    }
-    public static AuthResponse loadSaved()
-    {
-        string path = Application.persistentDataPath + "/player.info";
-        if (File.Exists(path))
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream stream = new FileStream(path,FileMode.Open);
-            AuthResponse data = bf.Deserialize(stream) as AuthResponse;
-            return data;
-        }
-        else
-        {
-            return null;
-        }
     }
 }
         
