59808cff838400768d7af7622c059e9f157b1a45
Lou-fr <122395695+Lou-fr@users.noreply.github.com>
2024-01-06 18:57:08 +0100

    Language and token Verification

--- Assets/Script/Login/TokenManager/TokenVerification.cs
+++ Assets/Script/Login/TokenManager/TokenVerification.cs
@@ -1,24 +1,25 @@
-using Newtonsoft.Json;
-using System.Text;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.Networking;
 using TokenManager;
 using Login.Library.Resonses;
 
-public class TokenVerification : MonoBehaviour
+namespace TokenVerfication
 {
-    private static string URL = @"https://localhost:7196/player/auth";
-    public static bool VerifToken()
+    public class TokenVerification : MonoBehaviour
     {
-        AuthResponse token =  TokenLoader.LoadToken();
-        var uwr = new UnityWebRequest(URL, "POST");
-        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-        uwr.SetRequestHeader("Content-Type", "application/json");
-        uwr.SetRequestHeader("Authorization", "Bearer " + token.Token);
-        uwr.SendWebRequest();
-        while (!uwr.isDone) Thread.Sleep(10);
-        if (uwr.result == UnityWebRequest.Result.Success) return true;
-        return false;
+        private static string URL = @"https://localhost:7196/player/auth";
+        public static (bool succes, string content) VerifToken()
+        {
+            AuthResponse token = TokenLoader.LoadToken();
+            var uwr = new UnityWebRequest(URL, "POST");
+            uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            uwr.SetRequestHeader("Content-Type", "application/json");
+            uwr.SetRequestHeader("Authorization", "Bearer " + token.Token);
+            uwr.SendWebRequest();
+            while (!uwr.isDone) Thread.Sleep(10);
+            if (uwr.result == UnityWebRequest.Result.Success) return (true, uwr.downloadHandler.text);
+            return (false, "");
+        }
     }
 }
