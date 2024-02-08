e2a8f155299c9fa64454b4f62af07cd2b41914f4
Lou-fr <122395695+Lou-fr@users.noreply.github.com>
2024-02-08 22:12:28 +0100

    In-Game Menu Update [NO-UI 0.0.3.5] + API Con
    (API)-Change Ip to an non local server & auth all certificate
    
    -Add graphics menu for phone
    
    -Better UI scalling
    
    -UI optimization

--- Assets/Script/In Game/Menu/Graphics.cs
+++ Assets/Script/In Game/Menu/Graphics.cs
@@ -8,14 +8,25 @@ using UnityEngine.UI;
 
 public class Graphics : MonoBehaviour
 {
-    [SerializeField] Toggle Vsync, F_S, fps;
-    [SerializeField] TMP_Dropdown Dresolution, D_FPS, DAAMode;
     [SerializeField] GameObject fps_counter;
     [SerializeField] UniversalAdditionalCameraData CameraData;
+
+#if UNITY_STANDALONE
+    private Toggle Vsync, F_S, fps;
+    private TMP_Dropdown Dresolution, D_FPS, DAAMode;
     Resolution[] resolutions;
     // Start is called before the first frame update
     void Start()
     {
+        Destroy(GameObject.Find("Gr_phone"));
+        Vsync = GameObject.Find("Gr_C_Vsync").GetComponentInChildren<Toggle>();
+        F_S = GameObject.Find("Gr_C_FS").GetComponentInChildren<Toggle>();
+        fps = GameObject.Find("Gr_C_See_FPS").GetComponentInChildren<Toggle>();
+        Dresolution = GameObject.Find("Gr_C_Resolution").GetComponentInChildren<TMP_Dropdown>();
+        D_FPS = GameObject.Find("Gr_C_FPS").GetComponentInChildren<TMP_Dropdown>();
+        DAAMode = GameObject.Find("Gr_C_AA").GetComponentInChildren<TMP_Dropdown>();
+        GameObject.Find("Gr_computer").SetActive(false);
+        GameObject.Find("Main_Menu_Panel").SetActive(false);
         F_S.isOn = Screen.fullScreen;
         if (QualitySettings.vSyncCount == 0)
         {
@@ -89,4 +100,55 @@ public class Graphics : MonoBehaviour
             Application.targetFrameRate = 30;
         }
     }
+#endif
+#if UNITY_ANDROID || UNITY_IOS
+    private Toggle fps;
+    private TMP_Dropdown D_FPS, DAAMode;
+    void Start()
+    {
+        Destroy(GameObject.Find("Gr_computer"));
+        fps = GameObject.Find("Gr_P_See_FPS").GetComponentInChildren<Toggle>();
+        D_FPS = GameObject.Find("Gr_P_FPS").GetComponentInChildren<TMP_Dropdown>();
+        DAAMode = GameObject.Find("Gr_P_AA").GetComponentInChildren<TMP_Dropdown>();
+        GameObject.Find("Gr_phone").SetActive(false);
+        GameObject.Find("Main_Menu_Panel").SetActive(false);
+        int CurrentIndex = 0;
+        for (AntialiasingMode i = 0; i < AntialiasingMode.TemporalAntiAliasing; i++)
+        {
+            if (i == CameraData.antialiasing)
+            {
+                CurrentIndex = ((int)i);
+            }
+        }
+        DAAMode.value = CurrentIndex;
+        if (Application.targetFrameRate == 60) D_FPS.value = 1;
+    }
+    public void ApplyPhoneChange()
+    {
+        fps_counter.SetActive(fps.isOn);
+        switch (DAAMode.value)
+        {
+            case 0:
+                CameraData.antialiasing = AntialiasingMode.None;
+                break;
+            case 1:
+                CameraData.antialiasing = AntialiasingMode.FastApproximateAntialiasing;
+                break;
+            case 2:
+                CameraData.antialiasing = AntialiasingMode.SubpixelMorphologicalAntiAliasing;
+                break;
+            case 3:
+                CameraData.antialiasing = AntialiasingMode.TemporalAntiAliasing;
+                break;
+        }
+        if (D_FPS.value == 1)
+        {
+            Application.targetFrameRate = 60;
+        }
+        else
+        {
+            Application.targetFrameRate = 30;
+        }
+    }
+#endif
 }
--- Assets/Script/MainMenu/TokenManage/TokenVerification.cs
+++ Assets/Script/MainMenu/TokenManage/TokenVerification.cs
@@ -7,10 +7,11 @@ namespace TokenManage
 {
     public class TokenVerification : MonoBehaviour
     {
-        private static string URL = @"https://localhost:7196/player/auth";
+        private static string URL = @"https://5.48.12.31:7196/player/auth";
         public static (bool succes, string content, UnityWebRequest.Result Response) VerifToken(Tokens token)
         {
             var uwr = new UnityWebRequest(URL, "POST");
+            uwr.certificateHandler = (CertificateHandler)new BypassCertificate();
             uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
             uwr.SetRequestHeader("Content-Type", "application/json");
             uwr.SetRequestHeader("Authorization", "Bearer " + token.Token);
