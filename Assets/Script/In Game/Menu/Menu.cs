5c4c721237d7805f1e45a1ecb886a948f8e69225
Lou-fr <122395695+Lou-fr@users.noreply.github.com>
2024-02-11 00:01:13 +0100

    In-Game Menu Update [NO-UI 0.0.5] & control Update [0.5.0]
    -Add joystick for mobile
    
    -add control for mobile
    
    -add controle page on controle option

--- Assets/Script/In Game/Menu/Menu.cs
+++ Assets/Script/In Game/Menu/Menu.cs
@@ -6,19 +6,27 @@ public class Menu : MonoBehaviour
 {
     [SerializeField] private GameObject panel;
     [SerializeField] private GameObject LanguagePanel;
-    [SerializeField] private GameObject CntrlPanel;
+    private GameObject CntrlPanel;
     private GameObject GraphicPanel;
     [SerializeField] private GameObject RTTMPopUp;
     [SerializeField] private GameObject QuitPopUp;
+    [SerializeField] private GameObject PhoneController;
     static bool GameIsPaused = false;
     static bool BtnSetPressed = false;
     public void Start()
     {
+
 #if UNITY_STANDALONE
+        Destroy(GameObject.Find("Cntrl_phone"));
         GraphicPanel = GameObject.Find("Gr_computer");
+        Destroy(PhoneController);
+        CntrlPanel = GameObject.Find("Cntrl_computer");CntrlPanel.SetActive(false);
+        Debug.Log(CntrlPanel.gameObject.name);
 #endif
 #if UNITY_ANDROID || UNITY_IOS
+        Destroy(GameObject.Find("Cntrl_computer"));
         GraphicPanel = GameObject.Find("Gr_phone");
+        CntrlPanel = GameObject.Find("Cntrl_phone");CntrlPanel.SetActive(false);
 #endif
     }
     public void Update()
@@ -119,6 +127,9 @@ public class Menu : MonoBehaviour
             QuitPopUp.SetActive(false);
             RTTMPopUp.SetActive(false);
         }
+#if UNITY_ANDROID || UNITY_IOS
+        PhoneController.SetActive(true );
+#endif
         panel.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
@@ -129,5 +140,8 @@ public class Menu : MonoBehaviour
         panel.SetActive(true);
         Time.timeScale = 0;
         GameIsPaused = true;
+#if UNITY_ANDROID || UNITY_IOS
+        PhoneController.SetActive(false);
+#endif
     }
 }
