05df8fcd7901fb4a414c194f5b7e08ddec402f4f
Lou-fr <pololulu2@outlook.fr>
2024-02-20 01:10:58 +0100

    Third person update [0.9.0]
    -add only when pressed rotate camera on computer

---
+++ Assets/Script/In Game/Player/ThirdPersonComputer.cs
@@ -0,0 +1,20 @@
+#if UNITY_STANDALONE
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ThirdPersonComputer : MonoBehaviour
+{
+    [SerializeField] private InputActionReference _look,_press;
+    private void Update()
+    {
+        if (_press.action.IsPressed())
+        {
+            _look.action.Enable();
+        }
+        else
+        {
+            _look.action.Disable();
+        }
+    }
+}
+#endif
