46da141bfd57d7f0b0b409d5e0af064c906cecdd
Lou-fr <pololulu2@outlook.fr>
2024-03-07 22:49:30 +0100

    Multiplayer controller [0.5.0]
    -Add multiplayer controller with mirror
    
    -Prepare for playfabApi Server API

--- Assets/Script/In Game/Player/thirdpersonmovement.cs
+++ Assets/Script/In Game/Player/thirdpersonmovement.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Mirror;
+using Cinemachine;
+using Unity.VisualScripting;
 
-public class thirdpersonmovement : MonoBehaviour
+
+public class thirdpersonmovement : NetworkBehaviour
 {
     [SerializeField] BleizInputManager _input;
     [SerializeField] private CharacterController controller;
@@ -14,35 +18,111 @@ public class thirdpersonmovement : MonoBehaviour
     [SerializeField] private Transform groundcheck;
     private float groundDistance = 0.4f;
     private float jumpHeight = 3f;
+    [SerializeField] private GameObject CinemachineCameraTarget;
     [SerializeField] private LayerMask groundmask;
     [SerializeField] private InputActionReference mouvement;
     [SerializeField] private InputActionReference jump;
     bool isGrounded;
+    private PlayerInput _playerInput;
+    private bool IsCurrentDeviceMouse
+#if UNITY_STANDALONE
+        = false;
+#else
+        =true;
+#endif
+
+    //cinemachine
+    [SerializeField] private InputActionReference _press;
+    public bool LockCameraPosition = false;
+    public float TopClamp = 70.0f;
+    public float BottomClamp = -30.0f;
+    private float _cinemachineTargetYaw;
+    private float _cinemachineTargetPitch;
+    private const float _threshold = 0.01f;
+    public float CameraAngleOverride = 0.0f;
+
+    public override void OnStartLocalPlayer()
+    {
+        cam = GameObject.FindAnyObjectByType<Camera>().GetComponent<Transform>();
+        GameObject.FindGameObjectWithTag("PlayerFollowCamera").GetComponent<CinemachineVirtualCamera>().Follow = transform.GetChild(0).transform;
+    }
+    public override void OnStartAuthority()
+    {
+        base.OnStartAuthority();
+
+        _playerInput = GetComponent<PlayerInput>();
+        _playerInput.enabled = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundcheck.position, groundDistance, groundmask);
-        if (isGrounded && velocity.y < 0)
+        if(!isLocalPlayer) return;
+        Grounded();
+        Move();
+    }
+    private void LateUpdate()
+    {
+        CameraRotation();
+    }
+    private void CameraRotation()
+    {
+        // if there is an input and camera position is not fixed
+        if (_input.LookInput.sqrMagnitude >= _threshold && !LockCameraPosition &&
+#if UNITY_STANDALONE 
+            _press.action.IsPressed() 
+#endif
+            )
         {
-            velocity.y = -2f;
+            //Don't multiply mouse input by Time.deltaTime;
+            float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+
+            _cinemachineTargetYaw += _input.LookInput.x * deltaTimeMultiplier;
+            _cinemachineTargetPitch += _input.LookInput.y * deltaTimeMultiplier;
         }
+
+        // clamp our rotations so our values are limited 360 degrees
+        _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+        _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+
+        // Cinemachine will follow this target
+        CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride, _cinemachineTargetYaw, 0.0f);
+    }
+
+    private void Move()
+    {
         Vector2 _movedir = _input.MoveInput;
         Vector3 direction = new Vector3(_movedir.x, 0f, _movedir.y).normalized;
-        if(direction.magnitude >= 0.1f)
+        if (direction.magnitude >= 0.1f)
         {
             float targatAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targatAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targatAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed *Time.deltaTime);
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-        if (_input.SpaceIsPressed.Equals(1) && isGrounded) 
+        if (_input.SpaceIsPressed.Equals(1) && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+    private void Grounded()
+    {
+        isGrounded = Physics.CheckSphere(groundcheck.position, groundDistance, groundmask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+    }
+     
+
+    private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+    {
+        if (lfAngle < -360f) lfAngle += 360f;
+        if (lfAngle > 360f) lfAngle -= 360f;
+        return Mathf.Clamp(lfAngle, lfMin, lfMax);
     }
 }
